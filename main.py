44d24583bf8a4a2c82b42bb8bbaf4358de205da8
junzhoude <jerry.jun.zhou@gmail.com>
2020-10-17 22:43:03 +0800

    2nd test commit

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-print('This is a test!!')
+import holidays
+
+de_bw_holidays = holidays.Germany(state='BW',years=2021).items()
+for date, name in sorted(de_bw_holidays):
+    print(date, name)
+
+
+
+
+
+
+
+
+
+
 
 
 
